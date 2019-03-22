80a4596a8ed793bfee62e0f44485400edd615843
Ken Zhou <mailofrealcev@gmail.com>
2019-03-22 16:00:49 -0400

    saves!

--- index.js
+++ index.js
@@ -6,30 +6,32 @@ function Site(w, h, canvas) {
     this.canvas.ctx = this.canvas.getContext('2d');
     this.canvas.width = this.width * 5;
     this.canvas.height = this.height * 5;
-    this.Pixel = function (x, y, color, size) {
+    this.Pixel = function(x, y, color, size) {
         this.x = x * size;
         this.y = y * size;
         this.color = color;
         this.size = size;
-        this.display = function () {
+        this.display = function() {
             that.canvas.ctx.fillStyle = color;
             that.canvas.ctx.fillRect(this.x, this.y, this.size, this.size);
         };
     };
-    this.getDataKey = function (x, y) {
+    this.getDataKey = function(x, y) {
         return x + ', ' + y;
     };
-    this.getCoordinatesFromDataKey = function (key) {
+    this.getCoordinatesFromDataKey = function(key) {
         var a = key.split(', ');
         return {
             x: Number(a[0]),
             y: Number(a[1])
         };
     };
-    this.data = [];
-    this.data.render = function () {
-        that.data.forEach(function (item, index) {
-            var x = Math.floor(index % that.width);
+    this.data = JSON.parse(window.localStorage.getItem('data')) || [];
+    this.data.render = function() {
+        that.data.forEach(function(item, index) {
+            if (item == null)
+                return;
+            var x = Math.floor(index % that.width) + 1;
             var y = Math.floor(index / that.width) + 1;
             var pixel = new that.Pixel(x, y, item, 5);
             pixel.display();
@@ -42,7 +44,7 @@ function Site(w, h, canvas) {
         red: 'rgb(244,67,54)',
         orange: 'rgb(255,152,0)',
         yellow: 'rgb(255,235,59)',
-        green: 'rgb(76,175,80)', 
+        green: 'rgb(76,175,80)',
         blue: 'rgb(33,150,243)',
         purple: 'rgb(156,39,176)',
         pink: 'rgb(233,30,99)'
@@ -50,28 +52,32 @@ function Site(w, h, canvas) {
 
     this.selectedColor = this.colors.black;
 
-    $('.palette-color').each(function(){
+    $('.palette-color').each(function() {
         this.style.background = that.colors[this.id];
-        $(this).click(function () {
+        $(this).click(function() {
             that.selectedColor = that.colors[this.id];
-            $('.palette-color').each(function () {
+            $('.palette-color').each(function() {
                 $(this).removeClass('palette-color-selected');
             });
             $(this).toggleClass('palette-color-selected');
         });
     });
 
-    this.canvas.addEventListener('click', function (e) {
-        var x = Math.floor(e.clientX / 5) + 1;
-        var y = Math.floor(e.clientY / 5) + 1;
+    this.canvas.addEventListener('click', function(e) {
+        var x = Math.floor(e.clientX / 5);
+        var y = Math.floor(e.clientY / 5);
         var index = Math.floor((y - 1) * that.width) + x;
         var pixel = new that.Pixel(x, y, that.selectedColor, 5);
         that.data[index] = pixel.color;
         that.data.render();
-        window.localStorage.setItem('data', that.data.toString());
+        window.localStorage.setItem('data', JSON.stringify(that.data));
     });
 
     this.data.render();
 }
 
 var site = new Site(window.innerWidth / 5, window.innerHeight / 5, document.querySelector('#main-canvas'));
+
+var app = firebase;
+var database = firebase.database();
+var databaseref = database.ref('space').child('main');
