74537e45b73558b02f403569787e45636540a100
Ken Zhou <mailofrealcev@gmail.com>
2019-04-03 16:55:16 -0400

    buggy zooming on zen

--- zen.js
+++ zen.js
@@ -1,8 +1,8 @@
 var scale = 1;
 var left = 0;
 var topPosition = 0;
-var x = 0;
-var y = 0;
+var x = window.innerWidth / 2;
+var y = window.innerHeight / 2;
 var mouseX = 0;
 var mouseY = 0;
 
@@ -10,16 +10,13 @@ document.body.onkeypress = function (e) {
     var key = e.key.toLowerCase();
     if (key === 'q') {
         scale *= 1.1;
-        site.canvas.style.transformOrigin = x + 'px ' + y + 'px';
+        site.canvas.style.transformOrigin = (x - left) + 'px ' + (y - topPosition) + 'px';
         site.canvas.style.transform = 'scale(' + scale + ')';
-        x = (mouseX * scale + left);
-        y = (mouseY * scale + topPosition);
     } else if (key === 'e') {
         scale *= 0.8;
-        site.canvas.style.transformOrigin = x + 'px ' + y + 'px';
+        y = (mouseY - topPosition);
+        site.canvas.style.transformOrigin = (x - left) + 'px ' + (y - topPosition) + 'px';
         site.canvas.style.transform = 'scale(' + scale + ')';
-        x = (mouseX * scale + left);
-        y = (mouseY * scale + topPosition);
     } else if (key === 'w') {
         topPosition += 40;
         site.canvas.style.top = topPosition + 'px';
@@ -34,10 +31,3 @@ document.body.onkeypress = function (e) {
         site.canvas.style.left = left + 'px';
     }
 };
-
-document.body.onmousemove = function (e) {
-    x = (e.clientX * scale + left);
-    y = (e.clientY * scale + topPosition);
-    mouseX = e.clientX;
-    mouseY = e.clientY;
-}
