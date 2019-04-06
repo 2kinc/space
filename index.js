e9b2bad5cde921f01492f41d31baca1bc58ec38d
Miracle Zhou <msdotnetclr@gmail.com>
2019-04-06 10:41:15 -0400

    beeeeeg rainbow

--- index.js
+++ index.js
@@ -172,7 +172,8 @@ function Site(space, canvas) {
                 }
             }
             if (user.traits.dev) {
-                p.style.animation = 'colors 10s infinite';
+                p.style.background = 'linear-gradient(124deg, #ff2400, #e81d1d, #e8b71d, #e3e81d, #1de840, #1ddde8, #2b1de8, #dd00f3, #dd00f3)';
+                p.style.animation = 'rainbow 18s infinite';
             }
             var span2 = document.createElement('span');
             span2.innerText = ' at ' + m.time;
